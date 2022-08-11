e5bc42f7afc30d3ff738a94f5ca80fc6d5c5db60
mmrzax <mmrzax@gmail.com>
2022-08-12 01:16:12 +0430

    feat: change NvimTree keymaps

--- .config/nvim/lua/mmrzax/tree.lua
+++ .config/nvim/lua/mmrzax/tree.lua
@@ -1,5 +1,34 @@
 local key = require("mmrzax.utils").keymap
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = { "l", "<CR>", "2-LeftMouse" }, action = "edit" },
+				{ key = "L", action = "cd" },
+				{ key = "b", action = "split" },
+				{ key = "v", action = "vsplit" },
+				{ key = "h", action = "close_node" },
+				{ key = "i", action = "preview" },
+				{ key = "I", action = "toggle_dotfiles" },
+				{ key = "R", action = "refresh" },
+				{ key = "a", action = "create" },
+				{ key = "D", action = "trash" },
+				{ key = "df", action = "remove" },
+				{ key = "r", action = "rename" },
+				{ key = "dd", action = "cut" },
+				{ key = "yy", action = "copy" },
+				{ key = "p", action = "paste" },
+				{ key = "yn", action = "copy_name" },
+				{ key = "yp", action = "copy_path" },
+				{ key = "ya", action = "copy_absolute_path" },
+				{ key = "-", action = "dir_up" },
+				{ key = "s", action = "system_open" },
+				{ key = "q", action = "close" },
+			},
+		},
+	},
+})
 
 key("n", "<leader>nn", ":NvimTreeToggle<CR>", { noremap = false })
