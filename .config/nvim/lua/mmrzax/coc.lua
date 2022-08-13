ebadad344c92006cec831918bc749fdae1d2e6df
mmrzax <mmrzax@gmail.com>
2022-08-13 20:51:35 +0430

    feat: add some keymap for coc

--- .config/nvim/lua/mmrzax/coc.lua
+++ .config/nvim/lua/mmrzax/coc.lua
@@ -4,23 +4,38 @@ vim.g.coc_node_path = os.getenv("NODE_PATH")
 
 vim.g.coc_global_extensions = {
 	"coc-yaml",
-	"coc-tsserver",
 	"coc-deno",
 	"coc-json",
 	"coc-html",
 }
 
--- highlight the symbol and its references when holding the cursor
-vim.api.nvim_create_autocmd("CursorHold", {
-	command = 'silent call CocActionAsync("highlight")',
-})
-
 key("i", "<c-space>", "coc#refresh()", { expr = true })
 
 key("n", "<leader>rn", "<Plug>(coc-rename)", { noremap = false })
 
--- use K to show documentation in preview window.
+key("n", "[g", "<Plug>(coc-diagnostic-prev)", { noremap = false })
+
+key("n", "]g", "<Plug>(coc-diagnostic-next)", { noremap = false })
+
+key("n", "gd", "<Plug>(coc-definition)", { noremap = false })
+
+key("n", "gy", "<Plug>(coc-type-definition)", { noremap = false })
+
+key("n", "gi", "<Plug>(coc-implementation)", { noremap = false })
+
+key("n", "gr", "<Plug>(coc-references)", { noremap = false })
+
+key("x", "<leader>f", "<Plug>(coc-format-selected)", { noremap = false })
+
+key("n", "<leader>f", "<Plug>(coc-format-selected)", { noremap = false })
+
 key("n", "K", ":call ShowDocumentation()<CR>")
+
+
+vim.cmd([[
+autocmd CursorHold * silent call CocActionAsync('highlight')
+]])
+
 vim.cmd([[
 function! ShowDocumentation()
 	if CocAction('hasProvider', 'hover')
