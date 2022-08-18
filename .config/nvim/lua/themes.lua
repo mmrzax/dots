4a11fb17bf63334dadaaaa5b4e125c65df7fcc88
mmrzax <mmrzax@gmail.com>
2022-08-19 00:09:35 +0430

    feat: change settings of nord theme

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -15,13 +15,14 @@ vim.g.gruvbox_italic = true
 vim.g.gruvbox_contrast_dark = "hard"
 
 -- nord
-vim.g.nord_italic_comments = true
-vim.g.nord_italic = true
-vim.g.nord_underline = true
-vim.g.nord_bold_vertical_split_line = true
 vim.g.nord_cursor_line_number_background = true
 vim.g.nord_uniform_status_lines = true
+vim.g.nord_bold_vertical_split_line = false
 vim.g.nord_uniform_diff_background = true
+vim.g.nord_bold = true
+vim.g.nord_italic = false
+vim.g.nord_italic_comments = false
+vim.g.nord_underline = true
 
 -- tokyonight
 vim.g.tokyonight_style = "night"
