8f864165be6484f8ae7a496f0ebea8525c5075af
mmrzax <mmrzax@gmail.com>
2022-08-19 00:40:57 +0430

    feat: update lsp setting

--- .config/nvim/lua/mmrzax/lspconfig.lua
+++ .config/nvim/lua/mmrzax/lspconfig.lua
@@ -3,7 +3,7 @@ DiagSigns = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = true,
   update_in_insert = false,
-  virtual_text = { spacing = 4, prefix = "●" },
+  -- virtual_text = { spacing = 4, prefix = "●" },
   severity_sort = true,
 })
 
