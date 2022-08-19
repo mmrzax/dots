8989f921f9fb5d0238a4ead9f0efd1bb8cf44b49
mmrzax <mmrzax@gmail.com>
2022-08-19 16:24:39 +0430

    feat: use cmp and delete pyright server

--- .config/nvim/lua/mmrzax/lspconfig.lua
+++ .config/nvim/lua/mmrzax/lspconfig.lua
@@ -1,5 +1,5 @@
 -- Diagnostics signs
-DiagSigns = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = true,
   update_in_insert = false,
@@ -7,27 +7,24 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
   severity_sort = true,
 })
 
-for type, icon in pairs(DiagSigns) do
+for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- Diagnostics mappings (See `:help vim.diagnostic.*` for documentation on any of the below functions)
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
+-- Use an on_attach function to only map the following keys after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  -- Mappings (See `:help vim.lsp.*` for documentation on any of the below functions)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
@@ -44,7 +41,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
   vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
   vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
-  vim.keymap.set("i", "<C-space>", "<C-x><C-o>", bufopts)
+  -- vim.keymap.set("i", "<C-space>", "<C-x><C-o>", bufopts)
 end
 
 local lsp_flags = {
@@ -52,8 +49,13 @@ local lsp_flags = {
   debounce_text_changes = 150,
 }
 
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- setup servers
+
 -- gopls
 require("lspconfig").gopls.setup({
+  capabilities = capabilities,
   on_attach = on_attach,
   flags = lsp_flags,
   settings = {
@@ -68,20 +70,16 @@ require("lspconfig").gopls.setup({
   },
 })
 
--- pyright
-require("lspconfig").pyright.setup({
-  on_attach = on_attach,
-  flags = lsp_flags,
-})
-
 -- tsserver
 require("lspconfig").tsserver.setup({
+  capabilities = capabilities,
   on_attach = on_attach,
   flags = lsp_flags,
 })
 
 -- sumneko_lua
 require("lspconfig").sumneko_lua.setup({
+  capabilities = capabilities,
   on_attach = on_attach,
   flags = lsp_flags,
   settings = {
@@ -110,4 +108,11 @@ require("lspconfig").sumneko_lua.setup({
       },
     },
   },
+})
+
+-- jsonls
+require("lspconfig").jsonls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  flags = lsp_flags,
 })
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -49,6 +49,21 @@ function M.setup()
       end,
     })
 
+    use({
+      "hrsh7th/nvim-cmp",
+      requires = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/vim-vsnip",
+      },
+      config = function()
+        require("mmrzax.cmp")
+      end,
+    })
+
     --[[ use({
       "neoclide/coc.nvim",
       branch = "release",
