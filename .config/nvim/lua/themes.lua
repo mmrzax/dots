71c3d795a9f21742dd7e92c230d597ba0eaa2b82
mmrzax <mmrzax@gmail.com>
2023-03-30 00:41:01 +0330

    feat: change colorscheme

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -114,11 +114,11 @@ require('material').setup({
 require('nightfox').setup({
   options = {
     styles = {
-      comments = "italic",
-      keywords = "bold",
-      types = "italic,bold",
+      -- comments = "italic",
+      -- keywords = "bold",
+      -- types = "italic,bold",
     }
   }
 })
 
-vim.cmd("colorscheme everforest")
+vim.cmd("colorscheme nightfox")
