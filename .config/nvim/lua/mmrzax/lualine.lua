7f63af0706fbc8088c0fe727a2eaf03263c13ca5
mmrzax <mmrzax@gmail.com>
2023-03-30 21:41:03 +0330

    feat: change theme

--- .config/nvim/lua/mmrzax/lualine.lua
+++ .config/nvim/lua/mmrzax/lualine.lua
@@ -1,7 +1,8 @@
 require("lualine").setup({
   options = {
     icons_enabled = true,
-    theme = "auto",
+    theme = "catppuccin",
+    -- theme = "auto",
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
     disabled_filetypes = { statusline = {}, winbar = {}, "NvimTree", "packer" },
