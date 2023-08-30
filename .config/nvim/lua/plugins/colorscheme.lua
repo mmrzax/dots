c9472ca1fad3c5f5db0601011b80ccf9003d0926
mmrzax <mmrzax@gmail.com>
2023-08-30 12:05:27 +0330

    feat: update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,13 @@
 return {
-    {
-        "rose-pine/neovim",
-        lazy = false,
-        priority = 1000,
-        name = "rose-pine",
+
+  { "rose-pine/neovim", lazy = false, priority = 1000, name = "rose-pine" },
+  { "sainnhe/everforest" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
     },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "rose-pine",
-      }
-    }
+  },
 }
