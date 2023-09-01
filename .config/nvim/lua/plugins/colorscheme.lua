cd2e26bae3a841e10e36eb9e7e92dd62c1a90f7a
mmrzax <mmrzax@gmail.com>
2023-09-01 17:33:23 +0330

    feat: add zenbones colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,21 @@
 return {
-
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
-    },
+    "rose-pine/neovim",
+    name = "rose-pine",
   },
 
-  { "rose-pine/neovim", lazy = false, priority = 1000, name = "rose-pine" },
-  { "sainnhe/everforest" },
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "sainnhe/everforest",
+  },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+  },
+
   {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
-
       color_overrides = {
         mocha = {
           base = "#000000",
@@ -22,6 +23,41 @@ return {
           crust = "#000000",
         },
       },
+    },
+  },
+
+  {
+    "mcchrish/zenbones.nvim",
+    priority = 1000,
+    dependencies = "rktjmp/lush.nvim",
+    init = function()
+      vim.o.termguicolors = true
+      vim.o.background = "dark"
+    end,
+    config = function()
+      local colors_name = "zenbones"
+      -- vim.g[colors_name .. "_compat"] = 1
+      vim.g[colors_name] = {
+        darkness = "stark",
+        -- lightness = "bright",
+        -- dim_noncurrent_window = true,
+        -- solid_vert_split = true,
+        -- solid_line_nr = true,
+        -- darken_non_text = 30,
+        italic_comments = false,
+        -- transparent_background = true,
+        -- lighten_cursor_line = 20,
+        -- darken_cursor_line = 20,
+        -- colorize_diagnostic_underline_text = true,
+      }
+      -- vim.cmd.colorscheme(colors_name)
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "zenbones",
     },
   },
 }
