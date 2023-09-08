419c502f0b2d50f908a51fb11f3d86cbc9ac33e4
mmrzax <mmrzax@gmail.com>
2023-09-08 15:13:31 +0330

    update

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "catppuccin/nvim", enabled = false },
+  { "SmiteshP/nvim-navic", enabled = false },
 }
