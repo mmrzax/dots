5e6e8ba236dfa8536f51b793b92c93dccf8e5729
mmrzax <mmrzax@gmail.com>
2023-09-22 08:28:20 +0330

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -62,6 +62,21 @@ config.keys = {
 	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 	{ key = "p", mods = "LEADER", action = act.ActivateCommandPalette },
 	{ key = "f", mods = "LEADER", action = act.Search({ CaseInSensitiveString = "" }) },
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 
 	{
 		key = "r",
