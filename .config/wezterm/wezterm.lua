114f1b8348d857ce76b28a6ff507289bdd756e77
mmrzax <mmrzax@gmail.com>
2023-09-22 07:41:04 +0330

    add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,115 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- Main Options
+config.color_scheme = "rose-pine"
+config.font = wezterm.font("CommitMono-MMRZAX")
+config.font_size = 16
+
+-- UI and Colors
+config.tab_bar_at_bottom = true
+local rosepine_base = "#191724"
+local rosepine_active_tab = {
+	bg_color = "#26233a",
+	fg_color = "#e0def4",
+}
+local rosepine_inactive_tab = {
+	bg_color = rosepine_base,
+	fg_color = "#6e6a86",
+}
+config.colors = {
+	tab_bar = {
+		background = rosepine_base,
+		active_tab = rosepine_active_tab,
+		inactive_tab = rosepine_inactive_tab,
+		inactive_tab_hover = rosepine_active_tab,
+		new_tab = rosepine_inactive_tab,
+		new_tab_hover = rosepine_active_tab,
+		inactive_tab_edge = "#6e6a86", -- (Fancy tab bar only)
+	},
+}
+config.window_frame = {
+	active_titlebar_bg = rosepine_base,
+	inactive_titlebar_bg = rosepine_base,
+}
+
+-- Key Binding and Key Tables
+wezterm.on("update-right-status", function(window, pane)
+	local name = window:active_key_table()
+	if name then
+		name = "TABLE: " .. name
+	end
+	window:set_right_status(name or "")
+end)
+
+config.leader = { key = "`", mods = "ALT" }
+config.keys = {
+	{ key = '"', mods = "LEADER|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "%", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
+
+	{
+		key = "a",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({
+			name = "activate_pane",
+			timeout_milliseconds = 1000,
+		}),
+	},
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+
+		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+
+		{ key = "Escape", action = "PopKeyTable" },
+	},
+
+	activate_pane = {
+		{ key = "LeftArrow", action = act.ActivatePaneDirection("Left") },
+		{ key = "h", action = act.ActivatePaneDirection("Left") },
+
+		{ key = "RightArrow", action = act.ActivatePaneDirection("Right") },
+		{ key = "l", action = act.ActivatePaneDirection("Right") },
+
+		{ key = "UpArrow", action = act.ActivatePaneDirection("Up") },
+		{ key = "k", action = act.ActivatePaneDirection("Up") },
+
+		{ key = "DownArrow", action = act.ActivatePaneDirection("Down") },
+		{ key = "j", action = act.ActivatePaneDirection("Down") },
+	},
+}
+
+return config
