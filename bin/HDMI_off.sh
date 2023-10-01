6ed5dad43b95fa3cbf59f0eda1fc320cfccf41fa
mmrzax <mmrzax@gmail.com>
2023-10-01 07:20:39 +0330

    delete unused scripts

--- bin/HDMI_off.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-A-0 --off --left-of eDP --output eDP --auto --primary
--- bin/eDP_off.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-A-0 --auto --primary --left-of eDP --output eDP --off
